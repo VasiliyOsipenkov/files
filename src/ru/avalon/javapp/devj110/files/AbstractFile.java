c5d47c9b5b3e28a8512bde7f653afdb298fe6ba8
Vasiliy Osipenkov <vasiliy.osipenkov@zyfra.com>
2022-11-02 13:25:43 +0300

    good1

--- src/ru/avalon/javapp/devj110/files/AbstractFile.java
+++ src/ru/avalon/javapp/devj110/files/AbstractFile.java
@@ -37,11 +37,89 @@ public abstract class AbstractFile {
 
     abstract String Details();
 
-    public static void printAll(AbstractFile [] files) {
+    /*public static void printAll(AbstractFile [] files) {
         System.out.println("File name; Size; Details");
 
         for (AbstractFile file : files) {
             System.out.println(file.getName() + ", " + file.getSize() + ", " + file.Details());
+        }
+
+    }*/
+    public static void printAll(AbstractFile [] files) {
+        int nameLength = 9,
+                sizeLength = 4,
+                detailsLength = 7;
+        for (AbstractFile file : files) {
+            if (file.name.length() > nameLength)
+                nameLength += file.name.length() - nameLength;
+            if (Integer.toString(file.size).length() > sizeLength)
+                sizeLength += Integer.toString(file.size).length() - sizeLength;
+            if (file.Details().length() > detailsLength)
+                detailsLength += file.Details().length() - detailsLength;
+        }
+        for (int i = nameLength + sizeLength + detailsLength + 2; i > 0; --i) {
+            System.out.print("-");
+        }
+
+        System.out.println();
+        for (int i = (nameLength - 9)/2; i > 0; --i) {
+            System.out.print(" ");
+        }
+        System.out.print("File name");
+        for (int i = (nameLength - 9)/2 + (nameLength - 9)%2; i > 0; --i) {
+            System.out.print(" ");
+        }
+        System.out.print("|");
+
+        for (int i = (sizeLength - 4)/2; i > 0; --i) {
+            System.out.print(" ");
+        }
+        System.out.print("Size");
+        for (int i = (sizeLength - 4)/2 + (sizeLength - 4)%2; i > 0; --i) {
+            System.out.print(" ");
+        }
+        System.out.print("|");
+
+        for (int i = (detailsLength - 7)/2; i > 0; --i) {
+            System.out.print(" ");
+        }
+        System.out.print("Details");
+        for (int i = (detailsLength - 7)/2; i > 0; --i) {
+            System.out.print(" ");
+        }
+        System.out.println();
+
+        for (int i = nameLength; i > 0; --i) {
+            System.out.print("-");
+        }
+        System.out.print("+");
+        for (int i = sizeLength; i > 0; --i) {
+            System.out.print("-");
+        }
+        System.out.print("+");
+        for (int i = detailsLength; i > 0; --i) {
+            System.out.print("-");
+        }
+        System.out.println();
+
+        for (AbstractFile file : files) {
+            System.out.print(file.getName());
+            for (int i = (nameLength - file.getName().length()); i > 0; --i) {
+                System.out.print(" ");
+            }
+            System.out.print("|");
+
+            for (int i = (sizeLength - Integer.toString(file.getSize()).length()); i > 0; --i) {
+                System.out.print(" ");
+            }
+            System.out.print(file.getSize());
+            System.out.print("|");
+
+            System.out.print(file.Details());
+            System.out.println();
+        }
+        for (int i = nameLength + sizeLength + detailsLength + 2; i > 0; --i) {
+            System.out.print("-");
         }
 
     }
--- src/ru/avalon/javapp/devj110/files/Main.java
+++ src/ru/avalon/javapp/devj110/files/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         AbstractFile [] files = {
-                new Docs("j110-lab2.docx", 232125,  "docx", 2),
+                new Docs("j110-lab2.pdf", 232125,  "pdf ", 2),
                 new Docs("j110-slides.pptx", 639616, "pptx", 8),
                 new Image("spb-map.png", 7030527, "image", "2048x6144"),
                 new Audio("06-PrettyGirl.mp3", 7893454, "audio", "Eric Clapton, Pretty Girl", "05:28"),
